bdfc1d80925aad3e8e2523517e8281f468c1e257
Timo Nachstedt <timo.nachstedt@googlemail.com>
2012-03-26 14:02:00 +0200

    added comment about nature of combination.h

--- controller/utils/ann-library/combination.h
+++ controller/utils/ann-library/combination.h
@@ -14,6 +14,11 @@
 class SO2CPG;
 class VRN;
 
+/**
+ * This is just an example of how to combine several networks into a parent
+ * network. DON'T USE this class in your project as it will be removed in the
+ * near future
+ */
 class Combination : public ANN
 {
 public:
