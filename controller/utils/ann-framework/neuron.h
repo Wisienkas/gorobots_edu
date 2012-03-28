c9b690e9b2586974c4493ef7e40ef301d4c64486
Timo Nachstedt <timo.nachstedt@googlemail.com>
2012-03-28 09:29:53 +0200

    added comments in neuron.h

--- controller/utils/ann-framework/neuron.h
+++ controller/utils/ann-framework/neuron.h
@@ -28,34 +28,200 @@ class ANN;
 class Synapse;
 //class TransferFunction;
 
+/**
+ * Time Discrete Artificial Neuron Class
+ *
+ * This class represents a single neuron in a time-discrete artificial neural
+ * net. It is described by its current output, activity, bias and input values
+ * as well as by the list of incoming and outgoing synapses and the transfer
+ * function to use for this neuron.
+ */
 class Neuron {
 public:
+    /**
+     * the constructor
+     */
     Neuron();
+
+    /**
+     * the destructor
+     */
     ~Neuron();
+
+    /**
+     * Adds an entry to the list of incoming synapses
+     *
+     * This method adds the given synapse to the list of incoming synapses. This
+     * does also mean that the synapse will be deleted when the neuron is.
+     * This method is called from the synapse constructor, so you do not have
+     * to (and also should not) call it manually.
+     *
+     * @param synapse pointer to the new incoming synapse
+     */
     void addSynapseIn(Synapse * synapse);
+
+    /**
+     * Adds an entry to the list of outgoing synapses
+     *
+     * This method adds the given synapse to the list of outgoing synapses. This
+     * does also mean that the synapse will be deleted when the neuron is.
+     * This method is called from the synapse constructor, so you do not have
+     * to (and also should not) call it manually.
+     *
+     * @param synapse pointer to the new outgoing synapse
+     */
     void addSynapseOut(Synapse * synapse);
+
+    /**
+     * Returns activity
+     *
+     * This method returns the activity value of the neuron. The returned
+     * reference stays valid for the whole lifetime of the neuron.
+     *
+     * @return activity value
+     */
     const double& getActivity() const;
+
+    /**
+     * Returns bias
+     *
+     * This method returns the bias value of the neuron. The returned
+     * reference stays valid for the whole lifetime of the neuron.
+     *
+     * @return bias value
+     */
     const double& getBias() const;
+
+    /**
+     * Returns output
+     *
+     * This method returns the output value of the neuron. The returned
+     * reference stays valid for the whole lifetime of the neuron.
+     *
+     * @return output value
+     */
     const double& getOutput() const;
+
+    /**
+     * Returns incoming synapse from a given neuron
+     *
+     * This method can be used to get the synapse connecting the presynaptic
+     * neuron pre with this neuron. It is assumed that there are not two
+     * synapses connecting the same presynaptic and postsynaptic neurons. If
+     * there is no such synapse present NULL is returned.
+     *
+     * @param pre pointer to presynaptic neuron
+     * @return pointer to synapse or NULL if no such synapse exists
+     */
     Synapse* getSynapseFrom(Neuron const * pre) const;
+
+    /**
+     * Removes an entry from the list of incoming synapses
+     *
+     * This method removes the given synapse from the list of incoming synapses.
+     * This method is called from the synapse destructor, so you do not have
+     * to (and also should not) call it manually.
+     *
+     * @param synapse synapse to remove
+     */
     void removeSynapseIn(Synapse const * synapse);
+
+    /**
+     * Removes an entry from the list of outgoing synapses
+     *
+     * This method removes the given synapse from the list of outgoing synapses.
+     * This method is called from the synapse destructor, so you do not have
+     * to (and also should not) call it manually.
+     *
+     * @param synapse synapse to remove
+     */
     void removeSynapseOut(Synapse const * synapse);
+
+    /**
+     * Sets the activity
+     *
+     * This method sets the activity value of the neuron.
+     *
+     * @param aactivity new activity value
+     */
     void setActivity(const double& aactivity);
+
+    /**
+     * Sets the bis
+     *
+     * This method sets the bias value of the neuron.
+     *
+     * @param abias new bias value
+     */
     void setBias(const double& abias);
+
+    /**
+     * Sets the external input
+     *
+     * This method sets the external input value of the neuron.
+     *
+     * @param ainput new input value
+     */
     void setInput(const double& ainput);
+
+    /**
+     * Sets the output
+     *
+     * This method sets the output value of the neuron.
+     *
+     * @param aoutput new output value
+     */
     void setOutput(const double& aoutput);
+
+    /**
+     * Sets the transfer function
+     *
+     * This method defines the transfer function used for this neuron. You have
+     * the possibility to define individual transfer function for every neuron
+     * of a neural net. Derive a new class from the TransferFunction class to
+     * create customn transfer functions.
+     *
+     * @param afunction pointer to the new transferFunction object
+     */
     void setTransferFunction(TransferFunction const * const afunction);
+
+    /**
+     * Updates neural activity
+     *
+     * This method calculates and sets the new activity value of the neuron
+     * based on the external input, the bias and the weights and the presynaptic
+     * outputs of all incoming synapses.
+     */
     void updateActivity();
+
+    /**
+     * Updates neural output
+     *
+     * This method calculates and sets the new output value of the neuron
+     * based on the activity value and the transfer function.
+     */
     void updateOutput();
 private:
+    /** type for holding a list of synapses */
     typedef std::list<Synapse*> SynapseList;
+    /** output value of the neuron */
     double output;
+    /** activity value of the neuron */
     double activity;
+    /** bias value of the neuron */
     double bias;
+    /** input value of the neuron */
     double input;
+    /** list of outgoing synapses */
     SynapseList synapsesOut;
+    /** list of incoming synapses */
     SynapseList synapsesIn;
+    /** pointer to the used TransferFunction object */
     TransferFunction const * func;
+    /**
+     * pointer to static and constant TanhFunction object representing the
+     * default used transfer function
+     */
     static TanhFunction const * const tanhFunction;
 };
 
