44cc8b5f0b0e629f421dcfb3c5f2b7d16980f517
fhesse <fhesse@physik3.gwdg.de>
2012-05-01 23:30:12 +0200

    - keyboard control real and sim prepared for easy switching between amosII version 1 and version 2 - missing: the amos-version-specific code in KineController

---
+++ projects/demos/amosii/keyboardcontrol_real/main.cpp
@@ -0,0 +1,305 @@
+
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <curses.h>
+
+
+
+/*
+ *  Template/Example for main program communicating with real amosII using serial communication
+ *
+ * */
+
+
+
+
+#include <selforg/agent.h>
+#include <selforg/abstractrobot.h>
+
+#include <selforg/one2onewiring.h>
+#include <selforg/sinecontroller.h>
+//#include <ode_robots/amosiistdscalingwiring.h>
+
+
+
+// instead of tripodgait18dof.h you can add your controller here
+//#include "tripodgait18dof.h"
+#include "amosIIcontrol.h"
+
+#include "amosIIserialv1.h"  //serial interface to AMOSII version 1
+#include "amosIIserialv2.h"  //serial interface to AMOSII version 2
+
+
+#include <cmath>
+
+#include <stdio.h> // standard input / output functions
+#include <string.h> // string function definitions
+#include <unistd.h> // standard function definitions
+#include <fcntl.h> // File control definitions
+#include <errno.h> // Error number definitions
+#include <termios.h> // terminal control definitions
+#include <time.h>   // time calls
+
+
+using namespace lpzrobots;
+
+
+using namespace std;
+
+bool stop=0;
+double noise=.0;
+double realtimefactor=1;
+bool   singleline = true; // whether to display the state in a single line
+
+
+
+// Helper
+int contains(char **list, int len,  const char *str){	for(int i=0; i<len; i++){
+  if(strcmp(list[i],str) == 0) return i+1;
+}
+return 0;
+};
+
+int main(int argc, char** argv){
+  list<PlotOption> plotoptions;
+  int port = 1;
+
+  int index = contains(argv,argc,"-g");
+  if(index >0 && argc>index) {
+    plotoptions.push_back(PlotOption(GuiLogger, atoi(argv[index])));
+  }
+  if(contains(argv,argc,"-f")!=0) plotoptions.push_back(PlotOption(File));
+  if(contains(argv,argc,"-n")!=0) plotoptions.push_back(PlotOption(MatrixViz));
+  if(contains(argv,argc,"-l")!=0) singleline=false;
+  index = contains(argv,argc,"-p");
+  if(index >0 && argc>index)
+    port=atoi(argv[index]);
+  if(contains(argv,argc,"-h")!=0) {
+    printf("Usage: %s [-g N] [-f] [-n] [p PORT]\n",argv[0]);
+    printf("\t-g N\tstart guilogger with interval N\n\t-f\twrite logfile\n");
+    printf("\t-n\tstart neuronviz\n");
+    printf("\t-p PORT\t COM port number, default 1\n");
+    printf("\t-h\tdisplay this help\n");
+    exit(0);
+  }
+
+  GlobalData globaldata;
+  AbstractRobot* robot;
+  Agent* agent;
+  initializeConsole();
+
+  // Different controllers
+
+
+  //	// Standard Tripod controller
+  //	AbstractController* controller = new TripodGait18DOF();
+  //	// create wiring between robot and controller (sensor <--> motor)
+  //	// the TripodGait18DOF assumes a the normal walking range  (a limited one)
+  //	// and hence requires the AmosIIStdScalingWiring:
+  //	AbstractWiring* wiring = new AmosIIStdScalingWiring(new ColorUniformNoise(0.0));
+
+
+  // select which version of AMOSII should be used
+  bool use_amosii_version1 = true;
+  bool use_amosii_version2 = false;
+  AmosIIControl* controller;
+
+  if (use_amosii_version1 && !use_amosii_version2){
+    std::cout<<std::endl<<std::endl<<"AMOSII  VERSION 1 SELECTED!"<<std::endl<<std::endl;
+    // using amosii version 1
+    robot         = new AmosIISerialV1("/dev/ttyS0");     // using serial port
+    //robot         = new AmosIISerialV1("/dev/ttyUSB0");   // using USB-to-serial adapter
+
+    controller = new AmosIIControl(/*amos version*/1);// (in general a pointer to AbstractController* is used)
+  } else {
+    std::cout<<"select only one version of AMOSII !"<<std::endl;
+    assert(use_amosii_version1 != use_amosii_version2);
+  }
+
+  if (use_amosii_version2 && !use_amosii_version1){
+    std::cout<<std::endl<<std::endl<<"AMOSII  VERSION 1 SELECTED!"<<std::endl<<std::endl;
+    // using amosii version 2
+    robot         = new AmosIISerialV2("/dev/ttyS0");     // using serial port
+    //robot         = new AmosIISerialV2("/dev/ttyUSB0");   // using USB-to-serial adapter
+
+    controller = new AmosIIControl(/*amos version*/2); // (usually a pointer to AbstractController* is used)
+  } else {
+    std::cout<<"select only one version of AMOSII !"<<std::endl;
+    assert(use_amosii_version1 != use_amosii_version2);
+  }
+
+
+
+  // one2onewiring gives full range of robot actuators
+  AbstractWiring* wiring = new One2OneWiring(new WhiteUniformNoise(),true);
+
+
+  agent         = new Agent(plotoptions);
+  agent->init(controller, robot, wiring);
+  // if you like, you can keep track of the robot with the following line.
+  // this assumes that your robot returns its position, speed and orientation.
+  // agent->setTracMkOptions(TrackRobot(true,false,false, false,"systemtest"));
+
+  globaldata.agents.push_back(agent);
+  globaldata.configs.push_back(robot);
+  globaldata.configs.push_back(controller);
+
+
+  showParams(globaldata.configs);
+  printf("\nPress c to invoke parameter input shell\n");
+  printf("The output of the program is more fun then useful ;-).\n");
+  printf(" The number are the sensors and the position there value.\n");
+  printf(" You probably want to use the guilogger with e.g.: -g 10\n");
+
+  bool use_additional_keys = true;
+  if (!use_additional_keys){
+    cmd_handler_init();
+    long int t=0;
+    while(!stop){
+      agent->step(noise,t);
+
+      //		printf("after agent step\n");
+      //		std::cout<<"after agent step"<<std::endl;
+      if(control_c_pressed()){
+        //			printf("after pressing STRL-C\n");
+        //			std::cout<<"after agent step"<<std::endl;
+        if(!handleConsole(globaldata)){
+          stop=1;
+        }
+        cmd_end_input();
+      }
+      // int drawinterval = 10000;
+      // if(realtimefactor!=0){
+      //   drawinterval = max(1,int(6*realtimefactor));
+      // }
+      // if(t%drawinterval==0){
+      //   printRobot(robot);
+      //   usleep(60000);
+      // }
+      t++;
+    };
+  } else { // use additional keys, setting of parameters by pressing Control_C not possible
+    std::cout<<"started with walking = false"<<std::endl;
+    initscr();
+    cbreak();
+    //noecho();
+    intrflush(stdscr,FALSE);
+    keypad(stdscr,TRUE);
+    nodelay(stdscr,TRUE);
+    long int t=0;
+    bool walking = false;
+    // add in order to add a keystroke for stopping coming back
+    bool come_back = false;
+    while(!stop){
+
+      int key=0;
+
+      key = wgetch (stdscr);
+      if (key==115){
+        std::cout<<"S pressed"<<std::endl;
+        if (walking){
+          controller->control_adaptiveclimbing->tripod->stopWalking();
+          std::cout<<"stop now"<< std::endl;
+          walking=false;
+        } else{
+          controller->control_adaptiveclimbing->tripod->startWalking();
+          std::cout<<"start now"<< std::endl;
+          walking=true;
+        }
+      }
+
+
+      if (key==104){
+        controller->control_adaptiveclimbing->tripod->liftLeg();
+        std::cout<<"H pressed"<<std::endl;
+        std::cout<<"lift leg"<<std::endl;
+      }
+
+      if (key==106){
+        controller->control_adaptiveclimbing->tripod->lowerLeg();
+        std::cout<<"J pressed"<<std::endl;
+        std::cout<<"lower leg"<<std::endl;
+      }
+
+      if (key==98){
+        std::cout<<"B pressed"<<std::endl;
+        //					if (come_back){
+        //						controller->control_adaptiveclimbing->tripod->stopComeBack();
+        //						std::cout<<"stop come back"<<std::endl;
+        //						come_back=false;
+        //					} else {
+        controller->control_adaptiveclimbing->tripod->comeBack();
+        std::cout<<"lower leg and come back"<<std::endl;
+        //				come_back=true;
+        //				}
+      }
+
+
+      if (key==27){
+        std::cout<<"ESC pressed"<<std::endl;
+        stop=true;
+      }
+
+      if (key==108){ // Turn left
+        std::cout<<"L pressed"<<std::endl;
+        controller->control_adaptiveclimbing->tripod->turnLeft();
+        std::cout<<"start now"<< std::endl;
+      }
+
+
+      if (key==114){ // Turn right
+        std::cout<<"R pressed"<<std::endl;
+        controller->control_adaptiveclimbing->tripod->turnRight();
+        std::cout<<"start now"<< std::endl;
+      }
+
+      if (key==97){ // left side ward
+        std::cout<<"A pressed"<<std::endl;
+        controller->control_adaptiveclimbing->tripod->sidewardleft();
+        std::cout<<"start now"<< std::endl;
+      }
+
+
+      if (key==103){ // right side ward
+        std::cout<<"G pressed"<<std::endl;
+        controller->control_adaptiveclimbing->tripod->sidewardright();
+        std::cout<<"start now"<< std::endl;
+      }
+
+      if (key==121){ // Turn right side ward // STILL NOT WORKING this function
+        std::cout<<"Y pressed"<<std::endl;
+        controller->control_adaptiveclimbing->tripod->turnrightsideward();
+        std::cout<<"start now"<< std::endl;
+      }
+
+      if (key==119){ // Turn right side ward
+        std::cout<<"W pressed"<<std::endl;
+        controller->control_adaptiveclimbing->tripod->turnleftsideward();
+        std::cout<<"start now"<< std::endl;
+      }
+
+      agent->step(noise,t); // execute a step in controller and robot
+      t++;
+    }
+  }
+
+
+
+
+
+  delete robot;
+  delete agent;
+  closeConsole();
+  fprintf(stderr,"terminating\n");
+  // should clean up but what costs the world
+  return 0;
+}
+
+
+
---
+++ projects/demos/amosii/keyboardcontrol_sim/main.cpp
@@ -0,0 +1,246 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Timo Nachstedt                                  *
+ *    nachstedt@physik3.gwdg.de                                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log: main.cpp,v $                                                    *
+ *                                                                         *
+ ***************************************************************************/
+
+// include simulation environment stuff
+#include <ode_robots/simulation.h>
+// include agent (class for holding a robot, a controller and a wiring)
+#include <ode_robots/odeagent.h>
+// playground
+#include <ode_robots/playground.h>
+// simple wiring
+#include <selforg/one2onewiring.h>
+// the robot
+#include <ode_robots/amosII.h>
+// the controller
+#include "amosIIcontrol.h"
+// joint needed for fixation of the robot in the beginning
+#include <ode_robots/joint.h>
+
+#include <ode_robots/playground.h>
+
+class ThisSim : public lpzrobots::Simulation {
+  public:
+
+  ThisSim(){
+    addPaletteFile("colors/UrbanExtraColors.gpl");
+    addColorAliasFile("colors/UrbanColorSchema.txt");
+    setGroundTexture("Images/whiteground.jpg");
+    setTitle("ANN Framework example");
+  }
+
+  /**
+   * starting function (executed once at the beginning of the simulation loop)
+   */
+  virtual void start(const lpzrobots::OdeHandle& odeHandle,
+      const lpzrobots::OsgHandle& osgHandle,
+      lpzrobots::GlobalData& global) {
+
+    // set initial camera position
+    setCameraHomePos(
+        lpzrobots::Pos(-0.0114359, 6.66848, 0.922832),
+        lpzrobots::Pos(178.866, -7.43884, 0));
+
+    // set simulation parameters
+    global.odeConfig.setParam("controlinterval", 10);
+    global.odeConfig.setParam("simstepsize", 0.01);
+
+    // select which version of AMOSII should be used
+    bool use_amosii_version1 = true;
+    bool use_amosii_version2 = false;
+
+    if (use_amosii_version1 && !use_amosii_version2){
+      std::cout<<std::endl<<std::endl<<"AMOSII  VERSION 1 SELECTED!"<<std::endl<<std::endl;
+      // using amosii version 1
+      // Add amosII robot
+      lpzrobots::AmosIIConf myAmosIIConf = lpzrobots::AmosII::getAmosIIv1Conf();
+      myAmosIIConf.rubberFeet = true;
+      amos = new lpzrobots::AmosII(
+          odeHandle,
+          osgHandle.changeColor(lpzrobots::Color(1, 1, 1)),
+          myAmosIIConf, "AmosIIv1");
+
+      controller = new AmosIIControl(/*amos version*/1);
+    } else {
+      std::cout<<"select only one version of AMOSII !"<<std::endl;
+      assert(use_amosii_version1 != use_amosii_version2);
+    }
+
+    if (use_amosii_version2 && !use_amosii_version1){
+      std::cout<<std::endl<<std::endl<<"AMOSII  VERSION 2 SELECTED!"<<std::endl<<std::endl;
+      // using amosii version 2
+      // Add amosII robot
+      lpzrobots::AmosIIConf myAmosIIConf = lpzrobots::AmosII::getAmosIIv2Conf();
+      myAmosIIConf.rubberFeet = true;
+      amos = new lpzrobots::AmosII(
+          odeHandle,
+          osgHandle.changeColor(lpzrobots::Color(1, 1, 1)),
+          myAmosIIConf, "AmosIIv2");
+
+      controller = new AmosIIControl(/*amos version*/2);
+    } else {
+      std::cout<<"select only one version of AMOSII !"<<std::endl;
+      assert(use_amosii_version1 != use_amosii_version2);
+    }
+
+    // put amos a little bit in the air
+    amos->place(osg::Matrix::translate(.0, .0, 0.5));
+
+
+
+    // create wiring
+    One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise());
+
+    // create agent and init it with controller, robot and wiring
+    lpzrobots::OdeAgent* agent = new lpzrobots::OdeAgent(global);
+    agent->init(controller, amos, wiring);
+
+    // add playground
+    lpzrobots::Playground* playground
+    = new lpzrobots::Playground(odeHandle, osgHandle,
+        osg::Vec3(10, 0.2, 0.3));
+    playground->setPosition(osg::Vec3(0,0,0));
+    global.obstacles.push_back(playground);
+
+    // create a fixed joint to hold the robot in the air at the beginning
+    robotfixator = new lpzrobots::FixedJoint(
+        amos->getMainPrimitive(),
+        global.environment);
+    robotfixator->init(odeHandle, osgHandle, false);
+
+    // inform global variable over everything that happened:
+    global.configs.push_back(amos);
+    global.agents.push_back(agent);
+    global.configs.push_back(controller);
+
+    std::cout << "\n\n"
+        << "################################\n"
+        << "#   Press x to free amosII!    #\n"
+        << "################################\n"
+        << "\n" << std::endl;
+
+
+    std::cout<<"started with walking = false"<<std::endl<<std::endl;
+    walking = false;
+
+  }
+
+  /**
+   * add own key handling stuff here, just insert some case values
+   */
+  virtual bool command(const lpzrobots::OdeHandle&,
+      const lpzrobots::OsgHandle&,
+      lpzrobots::GlobalData& globalData,
+      int key,
+      bool down)
+  {
+    if (down) { // only when key is pressed, not when released
+      switch (char(key)) {
+        case 'x':
+          if (robotfixator) {
+            std::cout << "dropping robot" << std::endl;
+            delete robotfixator;
+            robotfixator = NULL;
+          }
+          break;
+        case 's':
+          std::cout<<"s pressed"<<std::endl;
+          if (walking){
+            controller->control_adaptiveclimbing->tripod->stopWalking();
+            std::cout<<"stop now"<< std::endl;
+            walking=false;
+          } else{
+            controller->control_adaptiveclimbing->tripod->startWalking();
+            std::cout<<"start now"<< std::endl;
+            walking=true;
+          }
+          break;
+        case 'j':
+          controller->control_adaptiveclimbing->tripod->lowerLeg();
+          std::cout<<"j pressed"<<std::endl;
+          std::cout<<"lower leg"<<std::endl;
+          break;
+        case 'b':
+          std::cout<<"b pressed"<<std::endl;
+          //          if (come_back){
+          //            controller->control_adaptiveclimbing->tripod->stopComeBack();
+          //            std::cout<<"stop come back"<<std::endl;
+          //            come_back=false;
+          //          } else {
+          controller->control_adaptiveclimbing->tripod->comeBack();
+          std::cout<<"lower leg and come back"<<std::endl;
+          //        come_back=true;
+          //        }
+          break;
+        case 'l': // Turn left
+          std::cout<<"l pressed"<<std::endl;
+          controller->control_adaptiveclimbing->tripod->turnLeft();
+          std::cout<<"start now"<< std::endl;
+          break;
+        case 'r':  // Turn right
+          std::cout<<"r pressed"<<std::endl;
+          controller->control_adaptiveclimbing->tripod->turnRight();
+          std::cout<<"start now"<< std::endl;
+          break;
+        case 'a': // left side ward
+          std::cout<<"a pressed"<<std::endl;
+          controller->control_adaptiveclimbing->tripod->sidewardleft();
+          std::cout<<"start now"<< std::endl;
+          break;
+        case 'g': // right side ward
+          std::cout<<"g pressed"<<std::endl;
+          controller->control_adaptiveclimbing->tripod->sidewardright();
+          std::cout<<"start now"<< std::endl;
+          break;
+        case 'y': // Turn right side ward // STILL NOT WORKING this function
+          std::cout<<"y pressed"<<std::endl;
+          controller->control_adaptiveclimbing->tripod->turnrightsideward();
+          std::cout<<"start now"<< std::endl;
+          break;
+        case 'w': // Turn right side ward
+          std::cout<<"w pressed"<<std::endl;
+          controller->control_adaptiveclimbing->tripod->turnleftsideward();
+          std::cout<<"start now"<< std::endl;
+          break;
+        default:
+          return false;
+          break;
+      }
+    }
+    return false;
+  }
+
+  protected:
+  lpzrobots::Joint* robotfixator;
+  AmosIIControl* controller;
+  lpzrobots::AmosII* amos;
+  bool walking;
+};
+
+
+int main(int argc, char **argv)
+{
+  ThisSim sim;
+  sim.setGroundTexture("Images/greenground.rgb");
+  return sim.run(argc, argv) ? 0 : 1;
+}
+
