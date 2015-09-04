3723c731d55c5f5ba59fd9054080cb51ba74677e
p.manoonpong <poramate@manoonpong.com>
2015-09-04 12:07:15 +0200

    Added CDbot serial interface

---
+++ utils/real_robots/cdbot/cdbotSensMotDef.h
@@ -0,0 +1,41 @@
+/*
+ *
+ * Created on September 4 2015
+ * by Poramate Manoonpong
+ *
+ *
+ */
+
+#ifndef CDBOTSENSMOTDEF
+#define CDBOTSENSMOTDEF
+
+enum cdbotSensorName
+{
+
+	//  \IR_LEFT2\   /IR_LEFT1/      \IR_RIGHT2\   /IR_RIGHT1/ //
+	//														   //
+	//														   //
+	//													       //
+
+
+	IR_RIGHT1 = 0,
+	IR_RIGHT2 = 1,
+	IR_LEFT1 = 2,
+	IR_LEFT2 = 3,
+	LIGHT_RIGHT = 4,
+	LIGHT_LEFT = 5,
+	SOUND_RIGHT = 6,
+	SOUND_LEFT = 7,
+	CDBOT_SENSOR_MAX=8
+
+};
+
+enum cdbotMotorName
+{
+	MOTOR_RIGHT = 0,
+	MOTOR_LEFT = 1,
+
+};
+
+
+#endif
