42a1f03371b7311b4da6b85e0cf8c3e091f51ffa
poma <poramate@manoonpong.com>
2015-11-10 00:24:29 +0100

    New dungbeetle controller for new dung beetle simulation (dung beetle/locomotion/sim and manipulation/sim need to be modified to the new dungbeetle simulation)

---
+++ controllers/dungbeetle/modular_neural_control/dung_beetle/dungBeetlecontrol.cpp
@@ -0,0 +1,177 @@
+/*
+ * dungBeetlecontrol.cpp
+ *
+ *  Created on: Oct 24, 2015
+ *      Author: giuliano
+ */
+
+
+
+
+//#include <selforg/controller_misc.h>
+#include <math.h>
+#include "dungBeetlecontrol.h"
+
+using namespace matrix;
+using namespace std;
+
+dungBeetlecontrol::dungBeetlecontrol() :
+  AbstractController("dungBeetlecontrol", "$Id: dungBeetlecontrol.cpp,v 0.1 $") {
+	initialize(2,false,false);
+}
+dungBeetlecontrol::dungBeetlecontrol(int dungBeetletype,bool mMCPGs,bool mMuscleModelisEnabled) :
+  AbstractController("dungBeetlecontrol", "$Id: dungBeetlecontrol.cpp,v 0.1 $") {
+	initialize(dungBeetletype, mMCPGs, mMuscleModelisEnabled);
+}
+void dungBeetlecontrol::initialize(int aAMOSversion,bool mMCPGs,bool mMuscleModelisEnabled)
+{
+
+  t = 0; // step counter
+  cpg=new plastic(0.2,0.2,0.2,0.04*2*3.14,1.01,0.01);//CPG
+  MCPGs=mMCPGs;
+  inputDerivative.push_back(0);
+  inputDerivative.push_back(0);
+
+ inputDerivative2.push_back(0);
+ inputDerivative2.push_back(0);
+ plot.open("/home/giuliano/Documents/plots/dungBeetle6legs.dat");//writing data to file.. change this to your directory
+ };
+
+dungBeetlecontrol::~dungBeetlecontrol() {
+
+}
+
+
+void dungBeetlecontrol::init(int sensornumber, int motornumber, RandGen* randGen) {
+
+  numbersensors = sensornumber;
+  numbermotors = motornumber;
+  x.resize(sensornumber);
+  //y.resize(AMOSII_MOTOR_MAX);
+
+}
+
+void stepNoLearning(const sensor*, int number_sensors, motor*, int number_motors) {
+     // empty
+   }
+
+/// performs one step (includes learning). Calulates motor commands from sensor inputs.
+void dungBeetlecontrol::step(const sensor* x_, int number_sensors, motor* y_, int number_motors) {
+
+  assert(number_sensors == numbersensors);
+  assert(number_motors == numbermotors);
+
+  //0) Sensor inputs/scaling  ----------------
+
+
+  for (unsigned int i = 0; i < DUNGBEETLE_SENSOR_MAX; i++) {
+    x.at(i) = x_[i];
+  }
+
+
+
+  //2) Neural locomotion control------
+
+  if(!MCPGs) //single CPG
+    {
+
+	  std::cout<< "timer"<< t << std::endl;
+	  cpg->update(0);
+	   	  	  //y_[17]=1;//back left 3j 1 extended
+
+	  double val=((cpg->getOut1()+0.2)/(0.4))*2-1;
+
+	  std::cout << val;
+	  inputDerivative[0]=inputDerivative[1];
+	  inputDerivative[1]=val;
+
+	  double der=inputDerivative.at(1)-inputDerivative.at(0);
+
+	  double input_secJ;
+
+	  if(der > 0 && val > 0)
+	  		input_secJ=val;
+	  	else
+	  		input_secJ=-1.4;
+
+      double pattern1TC=val;
+	  double pattern1CT=input_secJ;
+
+
+
+	  double pattern2TC=-pattern1TC;
+
+	  inputDerivative2[0]=inputDerivative2[1];
+	  inputDerivative2[1]=pattern2TC;
+	  double der2=inputDerivative2.at(1)-inputDerivative2.at(0);
+
+
+	  double pattern2CT;
+
+	  	  if(der2 > 0 && pattern2TC > 0)
+	  	  		pattern2CT=pattern2TC;
+	  	  	else
+	  	  		pattern2CT=-1.4;
+
+
+
+
+
+	  //left back
+	  y_[5]=1;
+	  y_[11]=1;
+	  y_[17]=1;
+	  //left middle
+	  y_[4]=1;
+	  y_[10]=1;
+	  y_[16]=1;
+
+
+	  //right back
+	  y_[2]=1;
+	  y_[8]=1;
+	  y_[14]=1;
+
+	  //right middle
+	  y_[1]=1;
+	  y_[7]=1;
+	  y_[13]=1;
+
+	  //top left
+	  y_[3]=1;
+	  y_[9]=0;
+	  y_[15]=val;
+
+	  //top right
+	  y_[0]=1;
+	  y_[6]=0;
+	  y_[12]=val;
+
+
+	  plot << t << " " << pattern1TC << " " << pattern1CT << " "<< pattern2TC << " " << pattern2CT << endl;
+
+
+
+  	  /***Don't touch****Set Motor outputs begin *******************/
+  	 for(unsigned int j=0; j<y_MCPGs.size();j++)
+  	 for(unsigned k=0; k< 3; k++)//index of angel joints
+  	 y_[j+6*k] = y_MCPGs.at(j).at(j+6*k);
+   }
+
+
+  // update step counter
+  t++;
+}
+;
+
+/** stores the controller values to a given file. */
+bool dungBeetlecontrol::store(FILE* f) const {
+  return true;
+}
+
+/** loads the controller values from a given file. */
+bool dungBeetlecontrol::restore(FILE* f) {
+  //	Configurable::parse(f);
+  return true;
+}
+
---
+++ projects/dungbeetle/locomotion_control/dung_beetle_sim/main.cpp
@@ -0,0 +1,214 @@
+
+// include simulation environment stuff
+#include <ode_robots/simulation.h>
+// include agent (class for holding a robot, a controller and a wiring)
+#include <ode_robots/odeagent.h>
+// playground
+#include <ode_robots/playground.h>
+// simple wiring
+#include <selforg/one2onewiring.h>
+// the robot
+//#include <ode_robots/dungBeetle.h>
+
+
+
+
+//#include "dungBeetle.h"
+
+
+#include <ode_robots/dungbeetle.h>
+
+
+// the controller
+//#include "controllers/dungBeetlecontrol.h"
+
+#include "controllers/dungbeetle/modular_neural_control/dung_beetle/dungBeetlecontrol.h"
+
+#include <ode_robots/joint.h>
+
+#include <ode_robots/passivesphere.h>
+#include <ode_robots/passivebox.h>
+#include <selforg/abstractcontroller.h>
+#include <ode_robots/color.h>
+#include <iostream>
+using namespace std;
+using namespace lpzrobots;
+std::vector<lpzrobots::AbstractObstacle*> obst;
+
+bool track = true;
+
+
+class ThisSim : public lpzrobots::Simulation {
+public:
+
+	ThisSim() {
+		addPaletteFile("colors/UrbanExtraColors.gpl");
+		addColorAliasFile("colors/UrbanColorSchema.txt");
+		// you can replace color mappings in your own file, see colors/UrbanColorSchema.txt
+		// addColorAliasFile("myColorSchema.txt");
+		setGroundTexture("Images/whiteground.jpg"); // gets its color from the schema
+
+
+	}
+
+	/**
+	 * starting function (executed once at the beginning of the simulation loop)
+	 */
+	virtual void start(const lpzrobots::OdeHandle& odeHandle, const lpzrobots::OsgHandle& osgHandle,
+			lpzrobots::GlobalData& global) {
+		// set initial camera position
+		setCameraHomePos(lpzrobots::Pos(-0.0114359, 6.66848, 0.922832), lpzrobots::Pos(178.866, -7.43884, 0));
+
+		// set simulation parameters
+		global.odeConfig.setParam("controlinterval", 10);
+		global.odeConfig.setParam("simstepsize", 0.01);
+		global.odeConfig.setParam("noise", 0.02); // 0.02
+
+		// add playgrounds for three different experiments
+		lpzrobots::OdeHandle playgroundHandle = odeHandle;
+		playgroundHandle.substance = lpzrobots::Substance(100.0, 0.0, 50.0, 0.0); //substance for playgrounds (NON-SLIPPERY!!!)
+		double steplength = 0.43;
+
+		//PLAYGROUND
+			lpzrobots::Playground* playground = new lpzrobots::Playground(playgroundHandle, osgHandle, osg::Vec3(2, 0.6,
+					0.07), 1, false);
+			playground->setTexture(0, 0, lpzrobots::TextureDescr("Images/wall_bw.jpg", -0.5, -3));
+			playground->setPosition(osg::Vec3(0, 0, .0));
+			global.obstacles.push_back(playground);
+
+		//PLAYGROUND
+
+		// Add dungBeetle robot
+		DungBeetleConf myDungBeetleConf = dungBeetle::getDefaultConf(1.0 /*_scale*/, 0 /*_useShoulder*/,
+				1 /*_useFoot*/, 1 /*_useBack*/);
+		myDungBeetleConf.rubberFeet = true;
+
+		lpzrobots::OdeHandle rodeHandle = odeHandle;
+		rodeHandle.substance = lpzrobots::Substance(3.0, 0.0, 50.0, 0.8);
+
+		//------------------- Link the sphere to the Goal Sensor by Ren---------------
+		for (unsigned int i = 0; i < obst.size(); i++) {
+			myDungBeetleConf.GoalSensor_references.push_back(obst.at(i)->getMainPrimitive());
+		}
+		//------------------- Link the sphere to the Goal Sensor by Ren---------------
+
+		dungBeetleRobot
+		= new dungBeetle(rodeHandle, osgHandle.changeColor(lpzrobots::Color(1, 1, 1)), myDungBeetleConf, "dungBeetleRobot");
+
+		// define the usage of the individual legs
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->L0, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->L1, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->L2, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->R0, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->R1, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->R2, dungBeetleRobot->LEG);
+
+		// put dungBeetle a little bit in the air
+		dungBeetleRobot->place(osg::Matrix::translate(.0, .0, 0.0) * osg::Matrix::rotate(M_PI / 180 * (-5), 0, 0, 1));
+
+
+		controller = new dungBeetlecontrol(/*dungBeetle*/1,/*MCPGs=true*/false,/*Muscle Model =true*/false);
+
+		// create wiring
+		One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise());
+
+		// create agent and init it with controller, robot and wiring
+		lpzrobots::OdeAgent* agent = new lpzrobots::OdeAgent(global);
+		agent->init(controller, dungBeetleRobot, wiring);
+
+		// Possibility to add tracking for robot
+		if (track)
+			agent->setTrackOptions(TrackRobot(true, false, false, true, "", 60)); // Display trace
+
+		// inform global variable over everything that happened:
+		global.configs.push_back(dungBeetleRobot);
+		global.agents.push_back(agent);
+		global.configs.push_back(controller);
+
+		std::cout << "\n\n" << "################################\n" << "#   Press x to free dungBeetle!    #\n"
+				<< "################################\n" << "\n\n" << std::endl;
+	}
+
+
+/**************************Reset Function***************************************************************/
+    	virtual bool restart(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global)
+   	{
+       	 	// inform global variable over everything that happened:
+        	global.configs.erase(global.configs.begin());
+
+        	delete dungBeetleRobot;
+        	//delete (agent);
+        	global.agents.pop_back();
+
+       // Add dungBeetle robot
+		DungBeetleConf myDungBeetleConf = dungBeetle::getDefaultConf(1.0 /*_scale*/, 0 /*_useShoulder*/,
+				1 /*_useFoot*/, 1 /*_useBack*/);
+		myDungBeetleConf.rubberFeet = true;
+
+		lpzrobots::OdeHandle rodeHandle = odeHandle;
+		rodeHandle.substance = lpzrobots::Substance(3.0, 0.0, 50.0, 0.8);
+
+		//------------------- Link the sphere to the Goal Sensor by Ren---------------
+		for (unsigned int i = 0; i < obst.size(); i++) {
+			myDungBeetleConf.GoalSensor_references.push_back(obst.at(i)->getMainPrimitive());
+		}
+		//------------------- Link the sphere to the Goal Sensor by Ren---------------
+
+		dungBeetleRobot
+		= new dungBeetle(rodeHandle, osgHandle.changeColor(lpzrobots::Color(1, 1, 1)), myDungBeetleConf, "dungBeetle");
+
+		// define the usage of the individual legs
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->L0, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->L1, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->L2, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->R0, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->R1, dungBeetleRobot->LEG);
+		dungBeetleRobot->setLegPosUsage(dungBeetleRobot->R2, dungBeetleRobot->LEG);
+
+		// put dungBeetle a little bit in the air
+		dungBeetleRobot->place(osg::Matrix::translate(.0, .0, 0.0) * osg::Matrix::rotate(M_PI / 180 * (-5), 0, 0, 1));
+
+       	 	//Create wiring
+        	One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise());
+
+        	// create agent and init it with controller, robot and wiring
+        	lpzrobots::OdeAgent* agent = new OdeAgent(global);
+        	agent->init(controller, dungBeetleRobot, wiring);
+
+        	// Possibility to add tracking for robot
+        	if (track) agent->setTrackOptions(TrackRobot(true, false, false, true, "", 60)); // Display trace
+
+        	// inform global variable over everything that happened:
+       	 	global.configs.push_back(dungBeetleRobot);
+        	global.agents.push_back(agent);
+        	global.configs.push_back(controller);
+
+        	return true;
+    	}
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//   optional additional callback function which is called every simulation step.
+	//   Called between physical simulation step and drawing.
+	//   @param draw indicates that objects are drawn in this timestep
+	//   @param pause always false (only called of simulation is running)
+	//   @param control indicates that robots have been controlled this timestep
+	////////////////////////////////////////////////////////////////////////////////////////////////////////
+	virtual void addCallback(lpzrobots::GlobalData& globalData, bool draw, bool pause, bool control) {
+
+		//-----------------------------------------------------------------------------------
+	}
+
+protected:
+	lpzrobots::Joint* robotfixator;
+	AbstractController* controller;
+	dungBeetle* dungBeetleRobot;
+
+};
+
+int main(int argc, char **argv) {
+	ThisSim sim;
+	sim.setGroundTexture("Images/greenground.rgb");
+	return sim.run(argc, argv) ? 0 : 1;
+}
+
