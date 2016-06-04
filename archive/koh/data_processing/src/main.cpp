977bb903284dd78b86096d951d3ebcdb625c7ba6
poma <poramate@manoonpong.com>
2016-06-05 01:27:01 +0200

    Updated for fish data training

--- archive/koh/data_processing/src/main.cpp
+++ archive/koh/data_processing/src/main.cpp
@@ -8,14 +8,14 @@ int main( int argc, char ** argv ) {
 
     if( argc == 1 ) { // XXX Manual mode
 
-        string inputPath                    = "resources/withoutCF/maturingsalmon/Input13_89.txt";
-        string targetPath                   = "resources/withoutCF/maturingsalmon/Target13_89.txt";
+        string inputPath                    = "resources/withoutCF/maturingsalmon/Input13_89.txt"; //"resources/emg_data/Input_emg_24576.txt";
+        string targetPath                   = "resources/withoutCF/maturingsalmon/Target13_89.txt"; //"resources/emg_data/Target_emg_24576.txt";
         string resultPath                   = "results/result.txt";
         string saveDir                      = "save_files/";
         unsigned int saveNum                = 1;
         unsigned int numberOfInputs         = 3;
         unsigned int numberOfOutputs        = 1;
-        unsigned int numberOfHiddenUnits    = 50;//50
+        unsigned int numberOfHiddenUnits    = 50;
         unsigned int learningMode           = 1;
         unsigned int internalNonlinearity   = 2;
         unsigned int outputNonlinearity     = 0;
@@ -23,7 +23,7 @@ int main( int argc, char ** argv ) {
         double internalSparsity             = 50.0;
         double learningRate                 = 0.99;
         double leak                         = 1.0; //0.3
-        double percentage_1                 = 85.0; //   85% of total data for "training" (save)
+        double percentage_1                 = 85.0; //  85% of total data for "training" (save)
         double percentage_2                 = 100.0; // 100% of the remaining data (15% of total) for "validationUnits" (save & final) and another 0% of the remaining data (15% of total)  for "testing" (not use!)
         unsigned int repetition             = 3;
         unsigned int iterationLimit         = 5000;
