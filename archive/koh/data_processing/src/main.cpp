be83d0b046fc83fe9089d5de0f86997fa23c3071
p.manoonpong <poramate@manoonpong.com>
2017-02-24 15:21:20 +0100

    Added ModelData for fish data experiments

--- archive/koh/data_processing/src/main.cpp
+++ archive/koh/data_processing/src/main.cpp
@@ -11,12 +11,12 @@ int main( int argc, char ** argv ) {
         //string inputPath                    = "resources/withoutCF/maturingsalmon/Input13_166_salmon_cod.txt";//"resources/withoutCF/maturingsalmon/Input13_89.txt"; //"resources/emg_data/Input_emg_24576.txt";
         //string targetPath                   = "resources/withoutCF/maturingsalmon/Target13_166_salmon_cod.txt";//"resources/withoutCF/maturingsalmon/Target13_89.txt"; //"resources/emg_data/Target_emg_24576.txt";
 
-    	string inputPath                    = "resources/fishdata_combinedmodels/Table1/ModelE/Input13_Data3_5_test_35_124full.txt";
-	    string targetPath                   = "resources/fishdata_combinedmodels/Table1/ModelE/Target_Data3_5_test_35_124full.txt";
+    	string inputPath                    = "resources/fishdata_combinedmodels/Table1/ModelData/Data5/Input13_Data5_test_15_percent_missing.txt";
+	    string targetPath                   = "resources/fishdata_combinedmodels/Table1/ModelData/Data5/Target_Data5_test_15_percent_missing.txt";
         string resultPath                   = "results/result.txt";
         string saveDir                      = "save_files/";
         unsigned int saveNum                = 1;
-        unsigned int numberOfInputs         = 2;//3;
+        unsigned int numberOfInputs         = 2;
         unsigned int numberOfOutputs        = 1;
         unsigned int numberOfHiddenUnits    = 50;
         unsigned int learningMode           = 1;
@@ -25,8 +25,8 @@ int main( int argc, char ** argv ) {
         double inputSparsity                = 50.0;
         double internalSparsity             = 50.0;
         double learningRate                 = 0.99;
-        double leak                         = 1.0; //0.3
-        double percentage_1                 = 13.0; //  85% of total data for "training" (save)
+        double leak                         = 1;
+        double percentage_1                 = 85.0; //  85% of total data for "training" (save)
         double percentage_2                 = 100.0; // 100% of the remaining data (15% of total) for "validationUnits" (save & final) and another 0% of the remaining data (15% of total)  for "testing" (not use!)
         unsigned int repetition             = 3;
         unsigned int iterationLimit         = 5000;
